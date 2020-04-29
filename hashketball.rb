4bfd5d1ec48db49f69fcce33ce8ec757c07c5edc
Matt Guttman <msguttman@gmail.com>
2020-04-29 21:58:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,9 +7,15 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => {
+        
       }
   },
     :away => {
+      :team_name => "Charlotte Hornets"
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        
+      }
 
     }
   
