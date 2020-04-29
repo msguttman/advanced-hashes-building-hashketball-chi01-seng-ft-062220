03dd1e41696fce611bcc9bdd4456435960a68baf
Matt Guttman <msguttman@gmail.com>
2020-04-29 22:10:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -105,14 +105,14 @@ def game_hash
         :slam_dunks => 0
         ]
         [:player_name => "Kemba Walker"
-        :number =>
-        :shoe =>
-        :points =>
-        :rebounds =>
-        :assists =>
-        :steals =>
-        :blocks =>
-        :slam_dunks =>
+        :number => "33",
+        :shoe => "15",
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12
         ]
       }
 
