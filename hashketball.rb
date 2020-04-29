c01db7b283284cea1c7391aa52d2efccd45d2590
Matt Guttman <msguttman@gmail.com>
2020-04-29 21:51:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,20 @@
 # Write your code here!
 
 def game_hash 
+  game_hash = {
+    :home =>  
+     {:team_name => "Brooklyn Nets",
+       :colors => ["Black", "White"],
+       :players => {
+
+     }
+
+
+
+}
+  :away => {
+
+  }
+}
+game_hash
+end
